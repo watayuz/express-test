f63486a162d6e36a93ae67bba024001b9cbbf4e6
watayuz <w-desu@i.softbank.jp>
2016-10-02 01:58:01 +0900

    アクセス時のlog出力とREADMEの更新

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3');
+var path = require('path');
+//ログ出力するpackage
+var morgan = require('morgan');
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+
+//アクセスがあったらログ出力
+app.use(morgan('short'));
+
 
 var PORT = process.env.PORT || 8080;
 
 var router = express.Router();
 
+//top page
+app.get('/', function(req, res){
+	//HTMLファイルをレンダリングする
+	res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+
 router.get('/', function(req, res){
-	res.json({message: 'hello'});
+	res.json( {message: 'hello'} );
 });
 
 app.use('/api', router);
