3e6aef9ff970ad3588311168be032f236e8528a3
watayuz <w-desu@i.softbank.jp>
2016-10-02 18:00:58 +0900

    iosの追加とWebAPiの作成

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -6,29 +8,45 @@ var path = require('path');
 //ログ出力するpackage
 var morgan = require('morgan');
 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-
-//アクセスがあったらログ出力
-app.use(morgan('short'));
-
-
 var PORT = process.env.PORT || 8080;
 
 var router = express.Router();
 
-//top page
-app.get('/', function(req, res){
-	//HTMLファイルをレンダリングする
-	res.sendFile(path.join(__dirname+'/index.html'));
+app.use(bodyParser.urlencoded( {extended : true} ));
+app.use(bodyParser.json());
+//アクセスがあったらログ出力
+app.use(morgan('short'));
+
+//Routeの設定
+// app.use('/api', router);
+
+app.get('/api', function(req, res){
+	res.contentType('application/json');
+	var json = {
+		"user1" : {
+			"name" : "mahesh", 
+			"password" : "password1",
+			"profession" : "teacher",
+			"id": 1
+		},
+
+		"user2" : {
+			"name" : "suresh",
+			"password" : "password2",
+			"profession" : "librarian",
+			"id": 2
+		},
+
+		"user3" : {
+			"name" : "ramesh",
+			"password" : "password3",
+			"profession" : "clerk",
+			"id": 3
+		}
+	};
+	// res.end(json);
+	res.end(JSON.stringify(json));
 });
-
-
-router.get('/', function(req, res){
-	res.json( {message: 'hello'} );
-});
-
-app.use('/api', router);
 
 app.listen(PORT);
 console.log('server is running port ' + PORT);
